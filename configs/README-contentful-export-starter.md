7cfea65b14c1c5b09217a5bc7216b6e22f313f01
felixbuchholz <felixbuchholz@gmx.net>
2021-01-20 21:43:50 +0100

    add contentful script

---
+++ configs/README-contentful-export-starter.md
@@ -0,0 +1,6 @@
+To export the xbge/direct-democracy starter content from contentful run the following command in this directory:
+
+``` shell
+contentful login
+contentful space export --config contentful-export-starter-config.json
+```
--- scripts/contentful-setup.js
+++ scripts/contentful-setup.js
@@ -5,27 +5,32 @@ const { execSync } = require("child_process");
 const promptConfig = () => {
   return inquirer.prompt([
     {
-      name: "managementToken",
-      type: "password",
-      message: "Enter your contentful management token / private access token.",
+      name: "useExistingSpace",
+      type: "confirm",
+      message:
+        "Do you have an existing contentful space you want to use? – Warning: All content will be overwritten!",
       validate: (value) => {
         if (value.length > 0) {
           return true;
         }
 
-        return "Please your contentful management token / private access token.";
+        return "Choose an option";
       },
     },
     {
       name: "organizationName",
       type: "input",
-      message: "Enter the name of your organization",
+      when: ({ useExistingSpace }) => {
+        return !useExistingSpace;
+      },
+      message:
+        "Enter a name for your contentful space. Could be your organization’s name or just 'main'",
       validate: (value) => {
         if (value.length > 0) {
           return true;
         }
 
-        return "Please enter the name of your organization.";
+        return "Please enter the name of your contentful space.";
       },
     },
   ]);
@@ -33,23 +38,25 @@ const promptConfig = () => {
 
 const run = async () => {
   try {
-    const { managementToken, organizationName } = await promptConfig();
+    const { useExistingSpace, organizationName } = await promptConfig();
 
-    // Create a new space
-    execSync(
-      `npx contentful space create --name ${organizationName} --management-token ${managementToken}`,
-      { stdio: "inherit" }
-    );
+    // Login to contentful
+    execSync(`npx contentful login`, { stdio: "inherit" });
+
+    if (!useExistingSpace) {
+      // Create a new space
+      execSync(`npx contentful space create --name ${organizationName}`, {
+        stdio: "inherit",
+      });
+    }
 
     // Set as default space
-    console.log(
-      "Confirm the space you just created as your default contentful space:"
-    );
+    console.log("Confirm the space you want to import the starter content to:");
     execSync(`npx contentful space use`, { stdio: "inherit" });
 
     // Import the contentful starter json file
     execSync(
-      `npx contentful space import --content-file ${__dirname}/../configs/contentful-starter.json`,
+      `npx contentful space import --content-file ${__dirname}/../configs/contentful-starter.json --environment-id master`,
       { stdio: "inherit" }
     );
   } catch (error) {
