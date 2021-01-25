15d0ef8a9aacb1a260206444cb3013719e87c8fe
Valioesi <Vali_Schagerl@web.de>
2021-01-25 19:04:34 +0100

    Use execsync instead of shell js

--- scripts/backend-deploy.js
+++ scripts/backend-deploy.js
@@ -1,9 +1,12 @@
 const shell = require("shelljs");
+const { execSync } = require("child_process");
 
 const run = async () => {
   // Execute backend deploy script (using backend module)
   shell.cd(`${process.cwd()}/backend`);
-  shell.exec("yarn run deploy");
+
+  // We need to use exec sync because shell.exec does not work with input
+  execSync("yarn run deploy", { stdio: "inherit" });
 };
 
 run();
