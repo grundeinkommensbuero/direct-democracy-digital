a61a07adb3a62bab2da79f857a9504c87130c458
felixbuchholz <felixbuchholz@gmx.net>
2021-01-27 15:20:35 +0100

    add safety logout, add local install of contentful cli

--- scripts/contentful-setup.js
+++ scripts/contentful-setup.js
@@ -5,10 +5,23 @@ const { execSync } = require("child_process");
 const promptConfig = () => {
   return inquirer.prompt([
     {
+      name: "hasMultipleContentfulAccounts",
+      type: "confirm",
+      message:
+        "Do you have multiple contentful accounts? (We’ll log you out of existing contentful accounts so you can make sure the right one will be used)",
+      validate: (value) => {
+        if (value.length > 0) {
+          return true;
+        }
+
+        return "Choose an option";
+      },
+    },
+    {
       name: "useExistingSpace",
       type: "confirm",
       message:
-        "Do you have an existing contentful space you want to use? – Warning: All content will be overwritten!",
+        "Do you have an existing contentful space you want to use? – Warning: All content will be overwritten! (Hint: If you reached the space limit of your contentful plan – free plan: 1 – you won’t be able to create another one)",
       validate: (value) => {
         if (value.length > 0) {
           return true;
@@ -24,7 +37,7 @@ const promptConfig = () => {
         return !useExistingSpace;
       },
       message:
-        "Enter a name for your contentful space. Could be your organization’s name or just 'main'",
+        "Enter a name for your contentful space. Could be your organization’s name or just 'main'.",
       validate: (value) => {
         if (value.length > 0) {
           return true;
@@ -38,25 +51,44 @@ const promptConfig = () => {
 
 const run = async () => {
   try {
-    const { useExistingSpace, organizationName } = await promptConfig();
+    const {
+      hasMultipleContentfulAccounts,
+      useExistingSpace,
+      organizationName,
+    } = await promptConfig();
+
+    shell.exec(`yarn add --dev contentful-cli`);
+
+    const runContentful = `node ${process.cwd()}/node_modules/.bin/contentful`;
+
+    // Safety logout
+    if (hasMultipleContentfulAccounts) {
+      execSync(`${runContentful} logout`, {
+        stdio: "inherit",
+      });
+    }
 
     // Login to contentful
-    execSync(`npx contentful login`, { stdio: "inherit" });
+    execSync(`${runContentful} login`, {
+      stdio: "inherit",
+    });
 
     if (!useExistingSpace) {
       // Create a new space
-      execSync(`npx contentful space create --name ${organizationName}`, {
+      execSync(`${runContentful} space create --name ${organizationName}`, {
         stdio: "inherit",
       });
     }
 
     // Set as default space
     console.log("Confirm the space you want to import the starter content to:");
-    execSync(`npx contentful space use`, { stdio: "inherit" });
+    execSync(`${runContentful} space use`, {
+      stdio: "inherit",
+    });
 
     // Import the contentful starter json file
     execSync(
-      `npx contentful space import --content-file ${__dirname}/../configs/contentful-starter.json --environment-id master`,
+      `${runContentful} space import --content-file ${__dirname}/../configs/contentful-starter.json --environment-id master`,
       { stdio: "inherit" }
     );
   } catch (error) {
