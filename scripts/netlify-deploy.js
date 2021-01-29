ad30b5726b3b440c586e7a8726beca80d729fcb8
felixbuchholz <felixbuchholz@gmx.net>
2021-01-29 16:00:06 +0100

    minimal netlify setup

--- scripts/netlify-deploy.js
+++ scripts/netlify-deploy.js
@@ -1,52 +1,47 @@
 const shell = require("shelljs");
-const inquirer = require("inquirer");
 const { execSync } = require("child_process");
-
-const promptConfig = () => {
-  return inquirer.prompt([
-    {
-      name: "hasMultipleContentfulAccounts",
-      type: "confirm",
-      message:
-        "Do you have multiple contentful accounts? (We’ll log you out of existing contentful accounts so you can make sure the right one will be used)",
-      validate: (value) => {
-        if (value.length > 0) {
-          return true;
-        }
-
-        return "Choose an option";
-      },
-    },
-  ]);
-};
 
 const run = async () => {
   try {
-    // const { hasMultipleContentfulAccounts } = await promptConfig();
     // Change the directory (netlify-cli needs this for the site link in .netlify)
     shell.cd("site");
 
     // Define the netlify executable path
     const runNetlify = `${__dirname}/../node_modules/.bin/netlify`;
 
+    // NOTE: or future improvement, if we want to use a netlify.toml file
+    // we need to be careful the path will always be correct here –
+    // netlify-cli reads the base dir in the toml always as a relative path to the project root
+    // which is probably different for testing than it is in use later
+
     // Copy the netlify.toml config file
-    shell.cp("-r", `${__dirname}/../configs/netlify.toml`, `${process.cwd()}`);
-    shell.sed("-i", "BASE_DIR", `/test/site`, `${process.cwd()}/netlify.toml`);
+    // shell.cp("-r", `${__dirname}/../configs/netlify.toml`, `${process.cwd()}`);
+    // const projectFolder = shell.exec("echo ${PWD##*/}").stdout;
+    // console.log("projectFolder: ", projectFolder.trim());
+
+    // shell.sed(
+    //   "-i",
+    //   "BASE_DIR",
+    //   `/${projectFolder}/site`,
+    //   `${process.cwd()}/netlify.toml`
+    // );
+    // End of netlify.toml config
+
+    // NOTE: for continous deployment we can import an .env-file to netlify like this:
+    // // Import .env variables
+    // shell.exec(`${runNetlify} env:import .env.production --replaceExisting`);
 
     // Login
     execSync(`${runNetlify} login --new`, {
       stdio: "inherit",
     });
 
-    // // Import .env variables
-    // shell.exec(`${runNetlify} env:import .env.production --replaceExisting`);
-
     // Build
     // 1. Netlify
     // shell.exec(`${runNetlify} build`);
     // OR
     // 2. Gatsby
-    shell.exec(`yarn gatsby build`);
+    shell.exec(`yarn build`);
 
     // Deploy
     execSync(`${runNetlify} deploy --prod`, {
