d924d38d5995a32d19e901ff75e587dcf86b7ffc
felixbuchholz <felixbuchholz@gmx.net>
2021-01-29 12:08:51 +0100

    handling env variables and re-runs of the script

--- scripts/contentful-setup.js
+++ scripts/contentful-setup.js
@@ -1,6 +1,11 @@
 const shell = require("shelljs");
 const inquirer = require("inquirer");
-const { execSync } = require("child_process");
+const { exec, execSync, fork, spawnSync } = require("child_process");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const secretsPath = `${__dirname}/../configs/secrets.json`;
+const envPath = `${process.cwd()}/site/.env`;
 
 const promptConfig = () => {
   return inquirer.prompt([
@@ -51,15 +56,14 @@ const promptConfig = () => {
 
 const run = async () => {
   try {
+    // Prompt
     const {
       hasMultipleContentfulAccounts,
       useExistingSpace,
       organizationName,
     } = await promptConfig();
 
-    shell.exec(`yarn add --dev contentful-cli`);
-
-    const runContentful = `node ${process.cwd()}/node_modules/.bin/contentful`;
+    const runContentful = `node ${__dirname}/../node_modules/.bin/contentful`;
 
     // Safety logout
     if (hasMultipleContentfulAccounts) {
@@ -91,8 +95,116 @@ const run = async () => {
       `${runContentful} space import --content-file ${__dirname}/../configs/contentful-starter.json --environment-id master`,
       { stdio: "inherit" }
     );
+
+    // Get the spaceId and the management token
+    const config = shell.exec("contentful config list", { silent: true })
+      .stdout;
+    const contentfulSpaceId = config
+      .match(/activeSpaceId:\s\S+/gm)[0]
+      .replace(/activeSpaceId:\s/, "");
+    const managementToken = config
+      .match(/managementToken:\s\S+/gm)[0]
+      .replace(/managementToken:\s/, "");
+
+    // Create the contentful access token
+    // https://www.contentful.com/developers/docs/references/content-management-api/#/reference/api-keys
+
+    const request = {
+      method: "POST",
+      body: JSON.stringify({
+        name: `Direct Democracy API Key`,
+        environments: [
+          {
+            sys: {
+              type: "Link",
+              linkType: "Environment",
+              id: "staging",
+            },
+          },
+        ],
+      }),
+      headers: {
+        "Content-Type": "application/vnd.contentful.management.v1+json",
+        Authorization: `Bearer ${managementToken}`,
+      },
+    };
+
+    const response = await fetch(
+      `https://api.contentful.com/spaces/${contentfulSpaceId}/api_keys`,
+      request
+    );
+
+    // First run of the script
+    // Success is status code 201.
+    console.log(response.status);
+
+    if (response.status === 201) {
+      const apiKeyData = await response.json();
+      const contentfulAccessToken = apiKeyData.accessToken;
+      const apiKeyId = apiKeyData.sys.id;
+      const contentfulEnvironment = "master";
+
+      const secrets = {
+        contentfulAccessToken,
+        apiKeyId,
+        contentfulSpaceId,
+        contentfulEnvironment,
+      };
+      fs.writeFileSync(secretsPath, JSON.stringify(secrets));
+      writeEnvVariables(secrets);
+      console.log(
+        "Success: contentful space id and access token have been created"
+      );
+      // Second and consecutive runs of the script
+    } else {
+      // Response status code 422 indicates that a token with this name
+      // has already been set for this space.
+      // In this case we don’t want recreate another access token or delete and recreate this one.
+      // We just read the token and related variables
+      // from the backup copy in configs/secrets.json
+      let secretsRaw;
+      try {
+        secretsRaw = fs.readFileSync(secretsPath, "utf8");
+      } catch {
+        throw "Error: Could not create contentful access token and couldn’t find backup copy. Please reset your contentful setup according to the documentation and restart this process.";
+      }
+      const secrets = JSON.parse(secretsRaw);
+      if (
+        // Really making sure the spaceId matches the currently selected space
+        // (only for edge cases)
+        secrets.contentfulSpaceId === contentfulSpaceId
+      ) {
+        if (secrets.contentfulAccessToken) {
+          console.log(
+            "Success: contentful space id and access token have already been set"
+          );
+          // Note: I omitted re-writing the .env-file here.
+          // We need to make sure the .env-file is recreated in the same fashion after multiple runs of the scripts
+          // especially when integrating this script with others that need to access the .env-file
+        } else {
+          throw "Error: Your contentful access token could not be created and a previous version could not be retrieved. Please reset your contentful setup according to the documentation and restart this process.";
+        }
+      } else {
+        throw "Error: There has been a mismatch in your contentful space ids. Please reset your contentful setup according to the documentation and restart this process.";
+      }
+    }
   } catch (error) {
     console.log("Ooops, something went wrong", error);
   }
 };
 run();
+
+// Note: The function to write the .env file probably has to be adjusted
+// to match the process of the other scripts when executing them in order
+// (also needs to consider re-runs, so appending might not be the best way)
+// Suggestion: one secrets.json-file that keeps track of the variables
+// and a utils.js file with exports for writing the env-Variables, for frontend, backend, admin-panal respectively
+const writeEnvVariables = (secrets) => {
+  const {
+    contentfulAccessToken,
+    contentfulSpaceId,
+    contentfulEnvironment,
+  } = secrets;
+  const envVariables = `CONTENTFUL_SPACE_ID='${contentfulSpaceId}'\nCONTENTFUL_ACCESS_TOKEN='${contentfulAccessToken}'\nCONTENTFUL_ENVIRONMENT='${contentfulEnvironment}'`;
+  fs.appendFileSync(envPath, envVariables);
+};
